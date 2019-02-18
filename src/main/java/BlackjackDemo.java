ffa24c20dc6cb124b1b9e6f054f2db453375e305
Jordan Begian <jordanbegian@Jordans-MacBook-Pro.local>
2019-02-18 15:54:19 -0500

    created askForAnotherCard method to 'hit' or 'stand'

--- src/main/java/BlackjackDemo.java
+++ src/main/java/BlackjackDemo.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BlackjackDemo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         //Testing building of deck
         DeckGenerator generator = new DeckGenerator();
@@ -9,31 +11,42 @@ public class BlackjackDemo {
         Dealer dealer = new Dealer();
         ArrayList<Card> deckOfCards = generator.makeADeckOfCards();
 
-        generator.showAllCardsInDeck(deckOfCards);
-        System.out.println(" ");
-        System.out.println(" ");
-        dealer.createHand(deckOfCards);
+//        generator.showAllCardsInDeck(deckOfCards);
+
         player.createHand(deckOfCards);
 
-        System.out.println(" ");
+
         player.showHand();
-        System.out.println(" ");
-        dealer.showDealerStartCard();
-        System.out.println(" ");
 
-        player.drawACard(deckOfCards);
-        player.showHand();
-        System.out.println(" ");
-        dealer.showDealerStartCard();
-        System.out.println(" ");
+        System.out.println("\n");
 
-        player.drawACard(deckOfCards);
-        player.showHand();
-        System.out.println(" ");
-        dealer.showDealerStartCard();
+        askForAnotherCard(deckOfCards, player, scanner);
 
 
+    }
 
+    public static void askForAnotherCard(ArrayList<Card> deckOfCards, Player player, Scanner scanner) {
+        boolean repeat = true;
+
+        do {
+            System.out.println("Please enter a number \n" +
+                    "1 - Hit \n" +
+                    "2 - Stand\n");
+
+            String userInput = scanner.nextLine();
+
+            switch (userInput) {
+                case "1":
+                    player.drawACard(deckOfCards);
+                    player.showHand();
+                    break;
+                case "2":
+                    //TODO total hand
+                    repeat = false;
+                    break;
+            }
+
+        } while (repeat);
 
     }
 }
